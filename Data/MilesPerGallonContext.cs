67aa9f03d5522647f13162c24a2f84cdd0125517
lindseyshepard <55165590+lindseyshepard@users.noreply.github.com>
2019-10-01 17:11:32 -0400

    Adding Folders
    adding folders to support the project

---
+++ Data/MilesPerGallonContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MilesPerGallon.Models
+{
+    public class MilesPerGallonContext : DbContext
+    {
+        public MilesPerGallonContext (DbContextOptions<MilesPerGallonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MilesPerGallon.Models.Car> Car { get; set; }
+    }
+}
---
+++ Migrations/20191001133711_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace MilesPerGallon.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    CarModel = table.Column<string>(nullable: true),
+                    MilesDriven = table.Column<float>(nullable: false),
+                    GallonsFilled = table.Column<float>(nullable: false),
+                    FillupDate = table.Column<DateTime>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
